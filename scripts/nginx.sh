5480b1c5934a467c2692eaaf4cb277d924451f0a
Do Minh Quang <quangdm961@gmail.com>
2024-06-18 14:46:28 +0700

    add CICD config files

---
+++ scripts/nginx.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/bash
+
+sudo systemctl daemon-reload
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/Biblioteck/nginx/nginx.conf /etc/nginx/sites-available/Bibliotech
+sudo ln -s /etc/nginx/sites-available/Bibliotech /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/Bibliotech /etc/nginx/sites-enabled
+#sudo nginx -t
+sudo gpasswd -a www-data ubuntu
+sudo systemctl restart nginx
+
