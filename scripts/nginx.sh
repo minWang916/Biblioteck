a1f46e02008547539d3a108c4fcf4f2b786dcdf1
Do Minh Quang <quangdm961@gmail.com>
2024-06-20 22:11:44 +0700

    Revert "add SSL certificate"
    This reverts commit ff55eabb9c31d6b5a4b50edc0db7eb40a3ddbbce.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -11,5 +11,3 @@ sudo ln -s /etc/nginx/sites-available/Bibliotech /etc/nginx/sites-enabled/
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
-sudo certbot --nginx -d 3.26.191.43
-sudo systemctl restart nginx
